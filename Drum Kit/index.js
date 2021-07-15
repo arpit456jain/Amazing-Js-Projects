2e080fb3ddb8e93ccdf7a4b401f07787b09daa71
ri-tae-ka <ritzthareja@gmail.com>
2021-07-15 12:51:16 +0530

    add drum kit

---
+++ Drum Kit/index.js
@@ -0,0 +1,81 @@
+//detecting button press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+//detecting keyboard press
+
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "a":
+      var kickbass = new Audio("sounds/kick-bass.mp3");
+      kickbass.play();
+      break;
+
+    case "s":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "d":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+var activeButton = document.querySelector("." + currentKey);
+
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+  activeButton.classList.remove("pressed");
+}, 100);
+
+}
