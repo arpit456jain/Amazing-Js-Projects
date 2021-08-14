06a90aa19fd1de816be24fff8ea56a031b61ffd7
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2021-08-14 10:46:14 +0530

    Fix : scroll to top button

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-//NavBar 
+//NavBar
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
 // scroll to top starts
-var scrolltop = $('#scroll-top');
+var scrolltop = $("#scroll-top");
 
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 80) {
-        scrolltop.addClass('active');
-    } else {
-        scrolltop.removeClass('active');
-    }
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 80) {
+    scrolltop.addClass("active");
+  } else {
+    scrolltop.removeClass("active");
+  }
 });
-    
-scrolltop.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '200');
+
+scrolltop.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "200");
 });
 // scroll to top ends
 
-fetch('style_links.json')
-    .then(response => response.json())
-    .then(data => {
-        let links = data;
+fetch("style_links.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let links = data;
 
-        for (let i = 0; i < links.length; i++) {
-            console.log(links.length);
-            let StyleBoxDiv = document.createElement('div');
-            StyleBoxDiv.innerHTML = `<div class="stylebox">
+    for (let i = 0; i < links.length; i++) {
+      console.log(links.length);
+      let StyleBoxDiv = document.createElement("div");
+      StyleBoxDiv.innerHTML = `<div class="stylebox">
             <div class="image">
                  <img src="/${links[i].name}/preview.png" alt="">
              </div>
             <p>${links[i].name}</p>
             <a target="_blank" href="${links[i].link}">View</a>
             </div>`;
-            document.getElementById('styles').appendChild(StyleBoxDiv);
-        };
-});
+      document.getElementById("styles").appendChild(StyleBoxDiv);
+    }
+  });
