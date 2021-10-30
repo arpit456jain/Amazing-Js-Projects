433bc8f12b803bbfafe5aad4574c4d37c8af122d
sidparashar2001 <siddharthparashar2001@gmail.com>
2021-10-30 17:10:20 +0530

    Added

---
+++ picture-in-picture/script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+
+  }
+}
+
+button.addEventListener('click', async () => {
+
+  button.disabled = true;
+
+  await videoElement.requestPictureInPicture();
+
+  button.disabled = false;
+});
+
+
+selectMediaStream();
