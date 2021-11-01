82acf57b152a90c9ae16a0d4a44cffc1835a5233
justkrimzz <nikhilchivukula@live.com>
2021-10-31 20:48:37 -0700

    Added Kelvin logic to Temp Converter

--- Temp Convert/script.js
+++ Temp Convert/script.js
@@ -1,14 +1,17 @@
 var cel = document.getElementById("cel");
 var fah = document.getElementById("fah");
+var kel = document.getElementById("kel");
 
 cel.addEventListener('input', function () {
     let celValue = this.value;
+    let kelValue = Number(celValue) + 273.15;
     let fahValue = (celValue * 9 / 5) + 32;
     if (!Number.isInteger(fahValue)) {
         // No. of Digits we want after decimal
         fahValue = fahValue.toFixed(4);
     }
     fah.value = fahValue;
+    kel.value = kelValue;
 });
 
 fah.addEventListener('input', function () {
@@ -18,8 +21,21 @@ fah.addEventListener('input', function () {
         celValue = celValue.toFixed(4);
     }
     cel.value = celValue;
+    let kelValue = Number(celValue) + 273.15;
+    kel.value = kelValue;
 });
 
+kel.addEventListener('input', function () {
+    let kelValue = this.value;
+    let celValue = kelValue - 273.15;
+    let fahValue = (celValue * 9 / 5) + 32;
+    if (!Number.isInteger(fahValue)) {
+        // No. of Digits we want after decimal
+        fahValue = fahValue.toFixed(4);
+    }
+    fah.value = fahValue;
+    cel.value = celValue;
+});
 
 
 function select() { }
