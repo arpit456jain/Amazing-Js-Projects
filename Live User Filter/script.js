d5e0e02d5b3e41ca3d8e6a16bfd85cec61677342
Avik Mukherjee <77090462+Avik-creator@users.noreply.github.com>
2022-03-27 15:50:09 +0530

    Update script.js

--- Live User Filter/script.js
+++ Live User Filter/script.js
@@ -17,7 +17,7 @@ async function getdata() {
         const li = document.createElement('li')
         list.push(li)
         li.innerHTML = `
-            <img src="${user.picture.large}" alt="$(user.name.first)"
+            <img src="${user.picture.large}" alt="$(user.name.first)">
             <div class="user-info">
                 <h4>${user.name.first} ${user.name.last}</h4>
                 <p>${user.location.city}, ${user.location.country}</p>
