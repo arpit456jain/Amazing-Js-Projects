25250f599ecd401d0a662595adf3f93744031c8b
ritweekraj2802 <67289913+ritweekraj2802@users.noreply.github.com>
2022-04-07 00:02:35 +0530

    Pet Rescue

---
+++ Pet Rescue/js/script.js
@@ -0,0 +1,38 @@
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5, // Scale from 1 (refreshed) to 10 (exhausted)
+    sleep: function () {
+      console.log(`${this.name} needs nap. Zzz...`);
+      this.isTired = 1;
+    },
+    play: function () {
+      if (this.isTired === 10) {
+        console.log("Too tired to play.");
+        this.sleep();
+      } else {
+        console.log(`Yay! ${this.name} loves to play!`);
+        this.isTired += 1;
+      }
+    }
+  };
+
+  return pet;
+};
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
+
+console.log(sora, clover, baxter, cleo, francine);
+
+clover.sleep();
+baxter.play();
+
+console.log(clover, baxter);
