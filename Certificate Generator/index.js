a153b0bff4135ea6ca6e01fc9160a970a0349551
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-05-03 12:47:38 +0530

    updated as per instructions given

--- Certificate Generator/index.js
+++ Certificate Generator/index.js
@@ -1,8 +1,12 @@
-// console.log("Hello there");
+
+var temp = document.getElementById("template")
+
+var tempvalue = temp.options[temp.selectedIndex].value;
+console.log(tempvalue);
 
 const submitBtn = document.getElementById("submit");
 const user = document.querySelector("#name");
-// // console.log(user.value);
+// console.log(user.value);
 submitBtn.addEventListener('click',()=>{
     const value = user.value;
     // alert(value);
@@ -12,12 +16,15 @@ submitBtn.addEventListener('click',()=>{
 
 const generatePDF = async(name) =>{
     const {PDFDocument, rgb} = PDFLib;
-    const exBytes = await fetch("./certificate-template.pdf").then((res)=>{
+    // const exBytes = await fetch("./certificate-template.pdf").then((res)=>{
+    //     return res.arrayBuffer();
+    // });
+    const exBytes = await fetch(`./${temp.options[temp.selectedIndex].value}.pdf`).then((res)=>{
         return res.arrayBuffer();
     });
     // console.log(exBytes);
 
-  
+   
    
 
 
@@ -34,6 +41,7 @@ const generatePDF = async(name) =>{
     firstPg.drawText(name,{
         x: 275,
         y:280,
+        // y: 250,
         size: 50,
         font: myFont,
         color: rgb(.16, .66, .88)
@@ -44,7 +52,7 @@ const generatePDF = async(name) =>{
     const uri = await pdfDoc.saveAsBase64({dataUri: true});
     // window.open(uri);
     // document.querySelector("#mypdf").src = uri;
-    saveAs(uri, "Codedamn Project Certificate.pdf", {autoBom: true});
+    saveAs(uri, "GSSoC'22 Participation Certificate.pdf", {autoBom: true});
     // var file = new file(
     //     [pdfBytes], "Codedamn Certificate.pdf", {
     //         type: "applications/pdf;charset = utf-8",
