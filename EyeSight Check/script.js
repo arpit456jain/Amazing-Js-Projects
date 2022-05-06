ffca8e26f3d41c4735801e47c31f49bf31ea29ae
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2022-05-06 16:46:29 +0530

    Update script.js

--- EyeSight Check/script.js
+++ EyeSight Check/script.js
@@ -178,15 +178,3 @@ function toggleTimer() {
 }
 
 reset();
-
-// var sorted = sightAll
-// 	.toLowerCase()
-// 	.replace(/\s/g, "")
-// 	.split(",")
-// 	.sort(function(a, b) {
-// 		if (a < b) return -1;
-// 		else if (a > b) return 1;
-// 		return 0;
-// 	});
-
-// console.log(sorted.join(", "));
